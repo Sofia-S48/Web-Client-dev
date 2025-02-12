a848e2729328990272b9062f46d234dd5fd57d19
Sofia-S48 <sofia.saldumbide@gmail.com>
2025-02-12 10:44:54 -0500

    Add files via upload

---
+++ class8/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("demo").style.fontSize = '70px';
+}
+function showCalcul() {
+    document.getElementById("myCalcul").innerHTML = '12';
+}
+function presentation(){
+    let name= 'sofia';
+    let age = 19;
+    let hobbie= 'Playing guitar, and playing video games';
+    console.log("name: " +name + " age: " + age + " hobbie: " + hobbie); 
+    
+}
